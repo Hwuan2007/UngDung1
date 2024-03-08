b420989804923136cd1c72fe579375a9c09d179d
Hwuan2007 <thquan200703@gmail.com>
2024-03-08 16:53:26 +0700

    xong 1234

--- Models/NhanVien.cs
+++ Models/NhanVien.cs
@@ -36,7 +36,7 @@ namespace DataAccess
     {
         
         private Random random = new Random();
-
+        //Tạo danh sach nhân viên ngẫu nhiên
         public List<NhanVien> nhanvienngaunhien()
         {
             
@@ -59,6 +59,7 @@ namespace DataAccess
             return danhSachNhanVien;
         }
 
+        //Tạo giới hạn cho ngày sinh 
         private string NgaySinh()
         {
             DateTime ngayBatDau = DateTime.Now.AddYears(-50);
@@ -67,6 +68,7 @@ namespace DataAccess
             DateTime ngaySinh = ngayBatDau.AddDays(soNgayNgauNhien).Date;
             return ngaySinh.ToString("dd/MM/yyyy");
         }
+        //đặt quy luật random sdt
         private string SoDienThoai()
         {
             return "0" + random.Next(100000000, 999999999).ToString();
